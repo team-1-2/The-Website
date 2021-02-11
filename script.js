8d8c313af52a3b6822ae122357a34d2f4462769f
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-11 15:46:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var address = document.getElementById("address");
 function openTab(){
+  if (address.value.includes("http://") || address.value.includes("https://")) {
+      a = address.value;
+  } else{
+      a = "https://" + address.value;
+  }
   var i;
   for (i = 0; i < 5; i++) {
-    window.open(address.value);
+    window.open(a);
   }
 }
