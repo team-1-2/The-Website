6e0310adf03129cf6011f35993c9a0dd16e045dd
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-12 10:18:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var address = document.getElementById("address");
+var slider = document.getElementById("slider");
+var output = document.getElementById("output");
+output.innerHTML = "Open URL" + slider.value + "times.";
+
 function openTab(){
   if (address.value.includes("http://") || address.value.includes("https://")) {
       a = address.value;
@@ -6,7 +10,7 @@ function openTab(){
       a = "https://" + address.value;
   }
   var i;
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < slider.value; i++) {
     window.open(a);
   }
 }
